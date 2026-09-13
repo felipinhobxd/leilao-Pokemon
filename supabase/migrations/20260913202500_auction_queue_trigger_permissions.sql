428e6ae9bd00b7ffa18216afbda85f9743c02b4d
Felipe Veçoski <85942255+felipinhobxd@users.noreply.github.com>
2026-09-13 17:23:16 -0300

    security: restrict auction queue trigger function

---
+++ supabase/migrations/20260913202500_auction_queue_trigger_permissions.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.sync_auction_publish_queue_status() from public, anon, authenticated;
+grant execute on function public.sync_auction_publish_queue_status() to service_role;
